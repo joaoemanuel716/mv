47811b2ed365a381ba6a66bf7d620f5433c77685
Acid_man6924 <114834464+joaoemanuel716@users.noreply.github.com>
2024-03-25 16:44:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("seguir").onclick = function() {clicar()} //mudar texto do botão
+
+function clicar(){
+    document.getElementById("seguir").innerHTML = "Seguindo"
+} 
+
+//document = html
+//getElementById = forma de conexão do JS com a id do HTML
+//.onclick = ação de clicar no elemento
+//function = seria a função, a instrução que o elemento terao ao ser clicado
+//{clicar()} = o nome da função em si
+//.innerHTML = esta identificando que o documento e um html
